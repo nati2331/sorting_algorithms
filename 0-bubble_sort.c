a38c0c70e1e887b9a2279518a4210db12f076cd0
nati2331 <natnael.mk2@gmail.com>
2023-12-17 00:32:03 -0800

    q0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble Sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, index;
+    int tmp = 0;
+
+    if (size < 2)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        for (index = 0; index < size - i - 1; index++)
+        {
+            if (array[index] > array[index + 1])
+            {
+                tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+}
